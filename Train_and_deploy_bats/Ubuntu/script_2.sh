3b6856ee0c84b8524e1b72a4aa7bea918dbe4afc
TegwynTwmffat <paddy@goatindustries.co.uk>
2019-11-11 10:31:26 +0000

    append output filename with confidence percent.

--- Train_and_deploy_bats/Ubuntu/script_2.sh
+++ Train_and_deploy_bats/Ubuntu/script_2.sh
@@ -23,17 +23,28 @@ if [ -f "$FILE" ]; then
   read line1 <&6
   read line2 <&6
   # printf "${GREEN} $line2 ${NC}\n"
-  batName=$(echo "$line2" | sed 's|[.",]||g')                                # remove some characters
+  batName=$(echo "$line2" | sed 's|[.",]||g')                                # remove some " characters
   # printf "${GREEN} $batName ${NC}\n"
   batName=$(printf '%s' "$batName" | sed 's/[0-9]//g')                       # remove digits
   batName=$(printf '%s' "$batName" | sed 's/ //g')                           # remove white spaces.
   batName=$(printf '%s' "$batName" | sed 's/[[:blank:]]//g')                 # remove tab spaces.
+
+  batConfidence=$(echo "$line2" | sed 's/.*[[:blank:]]//') 
+  # batConfidence2=$((10*$batConfidence))
+  # printf "${GREEN}Bat confidence value is: $batConfidence ${NC}\n"
+
+  batConfidence2=$(echo "scale=2; 100 * $batConfidence" | bc -l)
+  batConfidence3=$(echo "$batConfidence2" | sed 's/[.].*$//')
+
+  # printf "${GREEN}Bat confidence2 value is: $batConfidence2 ${NC}\n"
+  # printf "${GREEN}Bat confidence3 value is: $batConfidence3 ${NC}\n"
+
   # printf "${GREEN}Bat detected was a $batName ${NC}\n"
   exec 6<&-
 
   cd /home/paddy/Desktop/deploy_classifier/temp/
   newName=$(date +%F-%H:%M:%S)
-  newName2="${batName}_${newName}"
+  newName2="${batConfidence3}%_${batName}_${newName}"
   # printf "${GREEN}New name:  ${newName2} ${NC}\n"
   mv new_${iter}.wav ${newName2}.wav
 
